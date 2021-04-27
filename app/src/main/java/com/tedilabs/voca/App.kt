a14312ea26f46c0e3b325e15753e1cc1b801b094
Ion Jo <onemonsterceo@gmail.com>
2021-04-27 16:08:02 +0900

    Timber

--- app/src/main/java/com/tedilabs/voca/App.kt
+++ app/src/main/java/com/tedilabs/voca/App.kt
@@ -1,6 +1,15 @@
 package com.tedilabs.voca
 
 import android.app.Application
+import timber.log.Timber
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
